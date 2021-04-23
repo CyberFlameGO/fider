b505e590ca963e8d39377bbde475b3850ff95d2c
Paul McCarty <paul@x-plane.com>
2021-04-23 15:58:32 -0500

    Use correct filter for posts

--- app/services/sqlstore/postgres/post.go
+++ app/services/sqlstore/postgres/post.go
@@ -440,15 +440,17 @@ func querySinglePost(ctx context.Context, trx *dbx.Trx, query string, args ...in
 func buildPostQuery(user *models.User, filter string) string {
 	// Only show posts tagged public AND posts by users whose posts aren't default-hidden
 	// ... but also show a hidden-posts user their own posts.
-	visibilityCondition := fmt.Sprintf(`AND tags.is_public = true AND (u.status < %d)`, enum.UserActive)
+	visibilityCondition := fmt.Sprintf(`AND tags.is_public = true`)
+	activeFilter := filter + fmt.Sprintf(` AND (u.status < %d)`, enum.UserActive)
 	if user != nil && user.IsCollaborator() {
 		visibilityCondition = ``
+		activeFilter = filter
 	} else if user != nil && !user.IsCollaborator() {
-		visibilityCondition = fmt.Sprintf(`AND tags.is_public = true AND (u.status < %d OR u.id = %d)`, enum.UserActive, user.ID)
+		activeFilter = filter + fmt.Sprintf(` AND (u.status < %d OR u.id = %d)`, enum.UserActive, user.ID)
 	}
 	hasVotedSubQuery := "null"
 	if user != nil {
 		hasVotedSubQuery = fmt.Sprintf("(SELECT true FROM post_votes WHERE post_id = p.id AND u.id = %d)", user.ID)
 	}
-	return fmt.Sprintf(sqlSelectPostsWhere, visibilityCondition, hasVotedSubQuery, filter)
+	return fmt.Sprintf(sqlSelectPostsWhere, visibilityCondition, hasVotedSubQuery, activeFilter)
 }
